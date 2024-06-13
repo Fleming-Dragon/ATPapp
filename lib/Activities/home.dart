97cd99aacb34d394a80439e2b31ae509365e33e2
RushiRaj-001 <rushirajapure111@gmail.com>
2024-06-13 21:55:33 +0530

    basic home

--- lib/Activities/home.dart
+++ lib/Activities/home.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -11,7 +30,83 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Home Activity"),
+      appBar: AppBar(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('ATP', style: TextStyle(fontSize: 18)),
+            Text(
+              'Hello, Rushikesh Rajapure',
+              style: TextStyle(fontSize: 12),
+            ),
+          ],
+        ),
+        backgroundColor: Color.fromARGB(255, 33, 208, 243),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              // Add notification functionality here
+            },
+          ),
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        crossAxisSpacing: 16.0,
+        mainAxisSpacing: 16.0,
+        children: [
+          _buildGridItem(Icons.school, 'Courses', Colors.blue),
+          _buildGridItem(Icons.receipt, 'Test Series', Colors.green),
+          _buildGridItem(Icons.book, 'Book Store', Colors.orange),
+          _buildGridItem(Icons.book_online, 'E-Books', Colors.red),
+          _buildGridItem(Icons.quiz, 'Quiz', Colors.purple),
+          _buildGridItem(
+              Icons.assignment_turned_in, 'Free Weekly Tests', Colors.teal),
+          _buildGridItem(
+              Icons.video_collection, 'Youtube Videos', Colors.amber),
+          _buildGridItem(
+              Icons.library_books, 'Free Study Material', Colors.cyan),
+          _buildGridItem(Icons.menu_book, 'Syllabus', Colors.pink),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add functionality here
+          print('Button Pressed!');
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildGridItem(IconData icon, String title, Color color) {
+    return InkWell(
+      onTap: () {
+        // Handle tap on the grid item
+        print('$title pressed');
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: color.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: color.withOpacity(0.5), width: 1),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 50, color: color),
+            SizedBox(height: 10),
+            Text(
+              title,
+              style: TextStyle(
+                  fontSize: 16, fontWeight: FontWeight.bold, color: color),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:atpapp/Activities/home.dart';
-import 'package:atpapp/Activities/loading.dart';
+// import 'package:atpapp/Activities/LoginPage.dart';
 import 'package:atpapp/Activities/test.dart';
 import 'package:atpapp/Activities/practice.dart';
 import 'package:atpapp/Activities/chapters.dart';
 import 'package:atpapp/Activities/marks.dart';
 import 'package:atpapp/Activities/questions.dart';
-import 'package:atpapp/Activities/topics.dart';
+import 'package:atpapp/topics.dart';
 
 void main() {
   runApp(MaterialApp(
     routes: {
-      "/" : (context) => loading(),
-      "/home" : (context) => Home(),
-      "/test" : (context) =>test(),
-      "/practice" : (context) => Practice(),
-      "/topics" : (context) => topics(),
-      "/chapters" : (context) => chapters(),
-      "/questions" : (context) => questions(),
-      "/pMarks" : (context) => Marks(),
+      // "/": (context) => LoginPage(),
+      "/home": (context) => Home(),
+      "/test": (context) => test(),
+      "/practice": (context) => Practice(),
+      "/topics": (context) => topics(),
+      "/chapters": (context) => chapters(),
+      "/questions": (context) => questions(),
+      "/pMarks": (context) => Marks(),
     },
   ));
 }
