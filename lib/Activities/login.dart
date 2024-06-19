89803ab8979170841766737b4a8c40bb8f581866
RushiRaj-001 <rushirajapure111@gmail.com>
2024-06-19 20:45:19 +0530

    login with email

--- lib/Activities/login.dart
+++ lib/Activities/login.dart
@@ -1,55 +1,266 @@
 import 'package:flutter/material.dart';
-import 'package:sign_in_button/sign_in_button.dart';
-import 'package:your_app_name/services/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Auth
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home.dart';
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _registerEmailController = TextEditingController();
+  final _registerPasswordController = TextEditingController();
+  final _registerConfirmPasswordController = TextEditingController();
+  bool _isRegistering = false; // To track registration
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Login'),
+          backgroundColor: Colors.amber,
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Sign In'),
+              Tab(text: 'Register'),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: [
-            // Use local app logo
-            Image.asset(
-              "assets/img/logo.png",
-              width: 200,
-              height: 200,
+            _buildSignInForm(),
+            _buildRegisterForm(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignInForm() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
             ),
-            SizedBox(height: 20),
-            Text(
-              'ATP Masters',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
             ),
-            SizedBox(height: 40),
-            SignInButton(
-              Buttons.googleDark,
-              onPressed: () async {
-                User? user = await signWithGoogle();
-                if (user != null) {
-                  Navigator.pushReplacementNamed(context, '/home');
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Google sign-in failed')),
-                  );
-                }
-              },
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _handleEmailSignIn,
+              child: const Text('Sign in with Email'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _handleGoogleSignIn,
+              child: const Text('Sign in with Google'),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildRegisterForm() {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _registerEmailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _registerPasswordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            TextField(
+              controller: _registerConfirmPasswordController,
+              decoration: const InputDecoration(labelText: 'Confirm Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _handleRegistration,
+              child: _isRegistering
+                  ? const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              )
+                  : const Text('Register'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _handleEmailSignIn() async {
+    _showLoadingDialog(); // Show loading dialog
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      Navigator.of(context).pop(); // Hide loading dialog
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (context, animation1, animation2) => const Home(),
+          transitionsBuilder: (context, animation1, animation2, child) {
+            return FadeTransition(
+              opacity: animation1,
+              child: child,
+            );
+          },
+        ),
+      );
+    } catch (error) {
+      Navigator.of(context).pop(); // Hide loading dialog if error occurs
+      _showErrorDialog(error.toString()); // Show error dialog
+    }
+  }
+
+  Future<void> _handleGoogleSignIn() async {
+    _showLoadingDialog(); // Show loading dialog
+
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        Navigator.of(context).pop(); // Hide loading dialog if canceled
+        return; // Canceled sign in
+      }
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken,
+        accessToken: googleAuth.accessToken,
+      );
+
+      await _auth.signInWithCredential(credential);
+
+      Navigator.of(context).pop(); // Hide loading dialog
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (context, animation1, animation2) => const Home(),
+          transitionsBuilder: (context, animation1, animation2, child) {
+            return FadeTransition(
+              opacity: animation1,
+              child: child,
+            );
+          },
+        ),
+      );
+    } catch (error) {
+      Navigator.of(context).pop(); // Hide loading dialog if error occurs
+      _showErrorDialog(error.toString()); // Show error dialog
+    }
+  }
+
+  Future<void> _handleRegistration() async {
+    setState(() {
+      _isRegistering = true;
+    });
+
+    final email = _registerEmailController.text;
+    final password = _registerPasswordController.text;
+    final confirmPassword = _registerConfirmPasswordController.text;
+
+    if (password != confirmPassword) {
+      _showErrorDialog('Passwords do not match');
+      setState(() {
+        _isRegistering = false;
+      });
+      return;
+    }
+
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (context, animation1, animation2) => const Home(),
+          transitionsBuilder: (context, animation1, animation2, child) {
+            return FadeTransition(
+              opacity: animation1,
+              child: child,
+            );
+          },
+        ),
+      );
+    } catch (error) {
+      _showErrorDialog(error.toString()); // Show error dialog
+    }
+
+    setState(() {
+      _isRegistering = false;
+    });
+  }
+
+  void _showLoadingDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const AlertDialog(
+          content: SizedBox(
+            height: 50,
+            width: 50,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Error'),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:your_app_name/Activities/login.dart';
-import 'package:your_app_name/Activities/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Your App Name',
+      title: 'Flutter Firebase Auth',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Login(),
-        '/home': (context) => Home(),
-      },
+      home: const LoginPage(),
     );
   }
 }
